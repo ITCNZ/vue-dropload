2b92cdf4e185ff83b93bf91ced69e9aeeb11034f
zhouhaipeng <1527127028@qq.com>
2017-01-14 11:01:51 +0800

    修改入口文件

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-//加载公共样式
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import infiniteScroll from 'vue-infinite-scroll'
 import App from './App.vue'
-
 import news from "./components/news.vue"
 import dropload from "./components/dropload.vue"
 import down from "./components/down.vue"
-
+import comments from "./components/comments.vue"
 Vue.use(VueResource)
 Vue.use(VueRouter)
 Vue.use(Vuex)
@@ -20,7 +18,8 @@ var router = new VueRouter({
   routes: [
 	  { path: '/news', component: news },
 	  { path: '/dropload', component: dropload},
-	  { path: '/down', component: down }
+	  { path: '/down', component: down },
+	  { path: '/comments', component: comments }
   ]
 });
 
