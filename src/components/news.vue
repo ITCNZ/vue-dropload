b51bb8bf502b69df87201ce84ff97bea446bb4b9
zhouhaipeng <1527127028@qq.com>
2017-02-11 17:29:13 +0800

    新增轮播组件

--- src/components/news.vue
+++ src/components/news.vue
@@ -1,15 +1,82 @@
 <template>
   <div id="news">
-      这是新闻页面啦啦啦啦啊。。。。
+      <slider v-bind:optionsInit="optionsInit" v-bind:datas="datas"></slider>
+<!--       <input type="button" v-bind:value="demoFollowStatus" class="scBox" v-on:click="DEMO__VUEX_FOLLOW">
+      <p>{{ count }}</p>
+      <p>
+        <button @click="DEMO__VUEX_INCREMENT">+</button>
+      </p> -->
   </div>
 </template>
 
 <script>
+import slider from "./slider.vue"
+// import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
+//console.log(mapState);
+//console.log(mapGetters)
 export default {
-  name: 'news'
+  name: 'news',
+  data () {
+    return {
+      optionsInit: {
+        index: 2,
+        isAuto: true
+      },
+      datas: [
+        {
+           href: 'http://www.baidu.com',
+          title: '111',
+            img: 'http://www.heibaipig.com/demo/images/test/1.jpg'
+        },
+        {
+          href: 'http://www.pc6.com',
+          title: '222',
+            img: 'http://0.thumb.pc6.com/up/2016-5/20165199297.jpg'},
+        {
+          href: 'http://www.9ht.com',
+          title: '3333',
+            img: 'http://0.thumb.pc6.com/up/2016-4/201642814841.jpg'
+        },
+        {
+          href: 'http://www.qbaobei.com',
+          title: '4444',
+            img: 'http://m.pc6.com/public/img/20151202.jpg'
+        }
+      ]
+
+    }
+  },
+  components: {
+    slider
+  },
+  // computed: {
+  //   ...mapState({
+  //     //state: ({demo}) => demo,
+  //     mapStateFollow: ({demo}) => demo.demoFollow,
+  //     mapStateFollowPending: ({demo}) => demo.demoFollowPending,
+  //     count: ({demo}) => demo.count
+  //   }),
+  //   ...mapGetters(['demoFollowStatus'])
+  // },
+  // methods: {
+  //   ...mapMutations(['DEMO__VUEX_FOLLOW', 'DEMO__VUEX_INCREMENT']),
+  //   // 同步的改变状态
+  //   ...mapActions(['demoFollowAjax'])
+  // }
 }
 </script>
 
 <style>
-
+.scBox{
+  position: fixed;
+  right: 10px;
+  bottom: 50px;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  border: none;
+  font-size: 12px;
+  text-align: center;
+  line-height: 40px;
+}
 </style>
---
+++ src/components/slider.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="slide" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd" :style="{height: slideHeight + 'px'}">
+    <div class="slide-box">
+        <ul class="slide-ul" :style="{width: slideWidth + 'px', transform: 'translate3D(' + ulLeft + 'px ,0 ,0)'}">
+            <li v-for="(item, index) in datas" :style="{width: windowW + 'px'}">
+              <a :href="item.href" :title="item.title"><img :src="item.img"></a>
+            </li>
+        </ul>
+    </div>
+    <div class="title-box">
+      <p class="title">{{datas[options.index].title}}</p>
+      <span class="circle">
+        <em v-for="item in pages" :class="item-1 == options.index ? 'active' : ''"></em>
+      </span>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'slide',
+  props: ['optionsInit', 'datas'],
+  data () {
+  	return {
+      options: {
+        index: this.optionsInit.index || 0,// 初始项
+        isLoop:  this.optionsInit.isLoop || true,// 是否轮循
+        isAuto: this.optionsInit.isAuto || false,// 自动滚动
+        time:  this.optionsInit.time || 3000,// 滚动间隔
+        moveTime:  this.optionsInit.moveTime || 700,// 滑动持续多久可以切换
+        scale:  this.optionsInit.scale || 0.5// 宽高比
+      },
+      windowW: window.screen.availWidth,
+      ulLeft: 0,
+      touch: {
+        s: [],
+        d: ""
+      },
+      setInter:''// 定时器
+  	}
+  },
+  computed: {
+    slideHeight: function () {// 根据比例计算高度
+        return this.windowW * this.options.scale;
+    },
+    slideWidth: function () {// 计算ul宽度
+      return this.windowW * this.datas.length
+    },
+    pages: function () {// 获取页面个数
+      return this.datas.length;
+    }
+  },
+  created() {
+    this.autoPlay();// 自动滚动
+    this.ulLeft = -this.options.index * this.windowW;
+  },
+  methods: {
+    touchStart (e) {
+      this.touch.s[0] = e.targetTouches[0].pageX;
+      this.touch.s[1] = e.targetTouches[0].pageY;
+      this.touch.s[2] = (new Date()).getTime();
+      clearInterval(this.setInter);
+    },
+    touchMove (e) {
+       let a = Math.abs(e.targetTouches[0].pageX - this.touch.s[0]);
+       let b = Math.abs(e.targetTouches[0].pageY - this.touch.s[1]);
+        //console.log("dMove:" + this.touch.d)
+        if (a >= b && this.touch.d == "") {
+            this.touch.d = 1;// 左右
+        } else if (this.touch.d == "") {
+            this.touch.d = 0;// 上下或者偏上下
+        }
+        if (this.touch.d == 1) {// 左右滚动
+           e.preventDefault();
+           this.ulLeft = -this.options.index * this.windowW + e.targetTouches[0].pageX - this.touch.s[0];
+        }
+    },
+    touchEnd (e) {
+      if (this.touch.d == 1){
+          if ((new Date()).getTime() - this.touch.s[2] > this.moveTime){
+              if (e.changedTouches[0].pageX - this.touch.s[0] > this.windowW/3){
+                this.autoFn("right")
+              } else if (this.touch.s[0] - e.changedTouches[0].pageX > this.windowW/3){
+                this.autoFn("left")
+              } else {
+                this.autoFn("reset")
+              }
+          } else {
+              if (e.changedTouches[0].pageX > this.touch.s[0]){
+                this.autoFn("right");
+              } else if (this.touch.s[0] > e.changedTouches[0].pageX){
+                this.autoFn("left")
+              }
+          }
+      }
+      console.log(this.options.index)
+      this.touch.d = "";
+      this.autoPlay();
+    },
+    autoFn (c) {
+        if(c == "left") {
+            if(this.options.index >= this.pages - 1){
+              this.options.index = this.pages - 1
+            }else{
+              this.options.index ++
+            }
+            this.ulLeft = -this.windowW * this.options.index
+        }else if (c == "right") {
+            if (this.options.index <= 0) {
+              this.options.index = 0
+            } else {
+              this.options.index --
+            }
+            this.ulLeft = -this.windowW * this.options.index
+        }else if(c == "reset") {
+            this.ulLeft = -this.windowW * this.options.index;
+        }
+    },
+    autoPlay () {
+      let that = this;
+      if (this.options.isAuto) {
+        this.setInter = setInterval(function() {
+            if (that.options.index >= that.pages - 1){
+                that.options.index = 0;
+            } else {
+                that.options.index ++;
+            }
+            that.ulLeft = -that.windowW * that.options.index;
+        }, that.options.time);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.slide{clear:both;position:relative;z-index:0;width:100%;max-width:960px;max-height:480px;margin:0 auto 0.3em auto;overflow:hidden; margin-top:10px;}
+.slide-box{ width:100%;height:100%; margin:0 auto;overflow:hidden}
+.slide .slide-ul{z-index:1;width:10000px;-webkit-transition: -webkit-transform 350ms cubic-bezier(.165, .84, .44, 1);transition: transform 350ms cubic-bezier(.165, .84, .44, 1);}
+.slide .slide-ul li{float:left;display:block; height: 100%}
+.slide .slide-ul li img{display:block;width:100%;height:100%;}
+.slide .title-box{ width:90%; padding:0 5%; z-index:999; height:30px; line-height:30px; color:#fff; font-size:14px; background:rgba(0,0,0,.5); position:absolute; left:0; bottom:0;}
+.slide .circle{ float:right;}
+.slide .title{ float:left; display:inline-block;}
+.slide .circle em{ display:inline-block; width:5px; height:5px; margin-left:5px; background-color:#fff; border-radius:50%;}
+.slide .circle .active{ background-color:#f00}
+</style>
